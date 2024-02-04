21c851caf8f7a8098a31c758f0905fb0378be3c5
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-02-04 17:12:04 +0100

    outpout list of recipes with details

--- angular_course_first_project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular_course_first_project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Bouilla', 'this is bouilla', 'https://img.freepik.com/photos-gratuite/steak-boeuf-grille-tranches-servi-salade-verte-aux-oeufs_140725-5065.jpg?w=740&t=st=1707055013~exp=1707055613~hmac=bc6af0c93ac1fd464fe23ac0a18642415223910252386cc19c6e199aa1cc5e6d'),
+    new Recipe('Bouilla', 'this is bouilla', 'https://img.freepik.com/photos-gratuite/steak-boeuf-grille-tranches-servi-salade-verte-aux-oeufs_140725-5065.jpg?w=740&t=st=1707055013~exp=1707055613~hmac=bc6af0c93ac1fd464fe23ac0a18642415223910252386cc19c6e199aa1cc5e6d')
+  ];
 }
