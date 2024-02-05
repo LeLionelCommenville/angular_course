47c850a1fab366271b4832b11fc7293a0681246a
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-02-05 15:29:35 +0100

    test using local reference  @output et @viewchild

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,27 +1,28 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrl: './cockpit.component.css'
 })
+
 export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>;
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>;
-  newServerName = '';
-  newServerContent = '';
-
-  onAddServer() {
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput;
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
