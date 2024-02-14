dc8ee722b7502712ed0b11e67c65c64878ab803e
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-02-14 08:53:04 +0100

    testing operator

--- angular-course-obs/src/app/home/home.component.ts
+++ angular-course-obs/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
-import { Observable } from 'rxjs-compat';
+import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -11,28 +11,33 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
     // this.firstObsSubscription = interval(1000).subscribe(count => {
     //   console.log(count);
-    // })
-    const customIntervalObservable : Observable<number> =
-    Observable.create(observer => {
+    // });
+    const customIntervalObservable = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if (count === 4) {
+        if (count === 5) {
           observer.complete();
         }
-        if(count > 3) {
-          observer.error(new Error('count is greater 3'));
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'));
         }
-        count++
+        count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+
+    this.firstObsSubscription = customIntervalObservable.pipe(filter((data: number) => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
