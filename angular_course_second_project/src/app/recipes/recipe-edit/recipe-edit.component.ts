c11b165ef6835bcf0df81c0f253f25e335f28cd4
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-02-26 11:00:54 +0100

    add delete ingredient functionality

--- angular_course_second_project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ angular_course_second_project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -57,6 +57,10 @@ export class RecipeEditComponent {
     this.router.navigate(['../'], {relativeTo: this.route})
   }
 
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeDescription = '';
