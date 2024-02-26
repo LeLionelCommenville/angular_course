daef0da7add56e6561e0da0e92cd29a2aecbea32
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-02-26 17:42:46 +0100

    transforming response data

--- angular_course_second_project/src/app/recipes/recipe.service.ts
+++ angular_course_second_project/src/app/recipes/recipe.service.ts
@@ -7,18 +7,18 @@ import { Subject } from "rxjs";
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
-      new Ingredient('Apples', 5),
-      new Ingredient('Tomatoes', 20)
+  // private recipes: Recipe[] = [
+  //   new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
+  //     new Ingredient('Apples', 5),
+  //     new Ingredient('Tomatoes', 20)
 
-    ]),
-    new Recipe('An another Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
-      new Ingredient('Steak', 5),
-      new Ingredient('Orange', 20)
-    ])
-  ];
-
+  //   ]),
+  //   new Recipe('An another Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
+  //     new Ingredient('Steak', 5),
+  //     new Ingredient('Orange', 20)
+  //   ])
+  // ];
+  private recipes: Recipe[] = [];
   constructor(private slService: ShoppingListService) {
 
   }
--- angular_course_second_project/src/app/shared/data-storage.service.ts
+++ angular_course_second_project/src/app/shared/data-storage.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { RecipeService } from "../recipes/recipe.service";
-import { Recipe } from "../recipes/recipe.model";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -20,6 +22,14 @@ export class DataStorageService {
   fetchRecipes() {
     this.http
     .get<Recipe[]>('https://angular-course-a4ffb-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+    .pipe(map(recipes => {
+      return recipes.map(recipe => {
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients? recipe.ingredients : []
+        };
+      });
+    }))
     .subscribe(recipes => {
       this.recipeService.setRecipes(recipes);
     })
